2dfa15c378405970aad7ed27f31f83bade19bf50
Satya Krishna Ranga Rao <108000447+satya19977@users.noreply.github.com>
2024-05-13 15:08:45 -0400

    Update db.go

--- db/db.go
+++ db/db.go
@@ -1,1 +1,45 @@
+package db
+ 
+import (
+    "database/sql"
+    "fmt"
+    "io/ioutil"
+    _ "modernc.org/sqlite"
+)
+ 
+var DB *sql.DB
+ 
+func InitDB() {
+ 
+    db, err := sql.Open("sqlite", "api.sql")
+    if err != nil {
+        panic("Database could not connect: " + err.Error())
+    }
+ 
+    DB = db
 
+    // Run migration
+    err = runMigration()
+    if err != nil {
+        panic("Could not run migration: " + err.Error())
+    }
+
+    fmt.Println("Migration executed successfully!")
+ 
+}
+ 
+func runMigration() error {
+    // Read migration SQL file
+    migrationSQL, err := ioutil.ReadFile("migration.sql")
+    if err != nil {
+        return err
+    }
+
+    // Execute migration script
+    _, err = DB.Exec(string(migrationSQL))
+    if err != nil {
+        return err
+    }
+    return nil
+
+}
